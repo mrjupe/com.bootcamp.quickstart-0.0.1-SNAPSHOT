ffd6eb5a2cc92577cc55927a6820a55857a654a1
M Jupri Amin <mjupriamin@gmail.com>
2018-12-28 11:29:51 +0700

    Edit model account and transaction

--- src/main/java/com/bootcamp/model/Account.java
+++ src/main/java/com/bootcamp/model/Account.java
@@ -13,30 +13,47 @@ public class Account {
 	private Date opendate;
 	private float balance;
 	@ManyToOne
-	@JoinColumn(name="customerid")
+	@JoinColumn(name = "customerid")
 	private Customer customer;
-	
+
+	public Account() {
+	}
+
+	public Account(int accountnumber, Date opendate, float balance, Customer customer) {
+		this.accountnumber = accountnumber;
+		this.opendate = opendate;
+		this.balance = balance;
+		this.customer = customer;
+	}
+
 	public int getAccountnumber() {
 		return accountnumber;
 	}
+
 	public void setAccountnumber(int accountnumber) {
 		this.accountnumber = accountnumber;
 	}
+
 	public Date getOpendate() {
 		return opendate;
 	}
+
 	public void setOpendate(Date opendate) {
 		this.opendate = opendate;
 	}
+
 	public float getBalance() {
 		return balance;
 	}
+
 	public void setBalance(float balance) {
 		this.balance = balance;
 	}
+
 	public Customer getCustomer() {
 		return customer;
 	}
+
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
 	}
--- src/main/java/com/bootcamp/model/Transaction.java
+++ src/main/java/com/bootcamp/model/Transaction.java
@@ -11,43 +11,64 @@ import javax.persistence.ManyToOne;
 public class Transaction {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String type;
 	private float amount;
 	private String amountsign;
 	@ManyToOne
-	@JoinColumn(name="accountnumber")
+	@JoinColumn(name = "accountnumber")
 	private Account account;
+
+	public Transaction() {
+	}
+
+	public Transaction(int id, String type, float amount, String amountsign, Account account) {
+		this.id = id;
+		this.type = type;
+		this.amount = amount;
+		this.amountsign = amountsign;
+		this.account = account;
+	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
 	public float getAmount() {
 		return amount;
 	}
+
 	public void setAmount(float amount) {
 		this.amount = amount;
 	}
+
 	public String getAmountsign() {
 		return amountsign;
 	}
+
 	public void setAmountsign(String amountsign) {
 		this.amountsign = amountsign;
 	}
+
 	public Account getAccount() {
 		return account;
 	}
+
 	public void setAccount(Account account) {
 		this.account = account;
 	}
-	
+
 }
